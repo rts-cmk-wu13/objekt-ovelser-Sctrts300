622384a3e7b6488bda31af1e0573f93114ee140f
Sidsel Thomsen <U2469819@rts-365.dk>
2025-02-04 11:57:59 +0100

    Done

---
+++ javascript.js
@@ -0,0 +1,23 @@
+
+let person = {
+    firstname: "Ole",
+    lastname: "Erling",
+    age: "54",
+    hometown: "Roshi",
+    pet: "Klappeslange",
+    petname: "Hermand"
+}
+
+console.log(person["firstname"]);
+console.log(person["lastname"]);
+console.log(person["age"]);
+console.log(person["hometown"]);
+console.log(person["pet"]);
+console.log(person["petname"]);
+
+
+
+
+
+
+
